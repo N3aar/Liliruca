ae78e4368b8d1d207295bd993d4bb77ffa12aab6
Gabriel <bieltavares-adm@hotmail.com>
2020-09-06 00:02:44 -0400

    feat: Team Command

---
+++ src/commands/others/team.js
@@ -0,0 +1,40 @@
+const moment = require('moment')
+const LilirucaCommand = require('@structures/LilirucaCommand')
+const LilirucaEmbed = require('@structures/LilirucaEmbed')
+const { EMOJIS, TEAM } = require('@constants')
+
+const bold = string => `**${string}**`
+
+class Team extends LilirucaCommand {
+  constructor () {
+    super('team', {
+      aliases: ['tm'],
+      emoji: EMOJIS.key,
+      editable: true
+    })
+  }
+
+  exec ({ t, ct, util, guild }) {
+    const { me: bot } = guild
+
+    const team = TEAM.map(user => {
+      const tags = user.tags.join(' | ')
+      const joined = moment(user.joined).format('L')
+      const status = t(`commons:${user.status}`)
+
+      const name = `${user.emoji} ${user.name}`
+      const value = `${bold(tags)}\n${bold(joined)}\n${bold(status)}`
+
+      return { name, value }
+    })
+
+    const embed = new LilirucaEmbed()
+      .setFooter(bot.displayName, bot.user.displayAvatarURL())
+      .addFields(team)
+      .setTimestamp()
+
+    util.send(ct('success'), embed)
+  }
+}
+
+module.exports = Team
