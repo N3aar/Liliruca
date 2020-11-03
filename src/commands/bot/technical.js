226c6c33204fd4607313dc55ace97d5686ee0bbb
Gabriel Tavares <bieltavares-adm@hotmail.com>
2020-11-03 15:57:36 -0400

    Feat: Technical Command

---
+++ src/commands/bot/technical.js
@@ -0,0 +1,60 @@
+const LilirucaCommand = require('@structures/LilirucaCommand')
+const LilirucaEmbed = require('@structures/LilirucaEmbed')
+const { dependencies } = require('@package')
+const { EMOJIS: { wrench } } = require('@constants')
+
+class Technical extends LilirucaCommand {
+  constructor () {
+    super('technical', {
+      aliases: ['tech'],
+      emoji: wrench,
+      editable: true,
+      clientPermissions: 'EMBED_LINKS'
+    })
+  }
+
+  exec ({ ct, client, util }) {
+    const djs = dependencies['discord.js']
+    const akairo = dependencies['discord-akairo']
+    const memoryUsage = (process.memoryUsage().heapUsed / 1048576).toFixed(2)
+    const fields = [
+      {
+        name: '\\⚙️ Engine',
+        value: `**Node.js ${process.version}**`,
+        inline: true
+      },
+      {
+        name: `\\💾 ${ct('database')}`,
+        value: '**MongoDB**',
+        inline: true
+      },
+      {
+        name: `\\📊 ${ct('memoryUsage')}`,
+        value: `**${memoryUsage} mb**`,
+        inline: true
+      },
+      {
+        name: `\\📇 ${ct('events')}`,
+        value: `**${ct('eventCount', { count: client.eventCount })}**`,
+        inline: true
+      },
+      {
+        name: `\\📚 ${ct('library')}`,
+        value: `**Discord.js v${djs.replace('^', '')}**`,
+        inline: true
+      },
+      {
+        name: '\\📦 Framework',
+        value: `**Discord Akairo v${akairo.replace('^', '')}**`,
+        inline: true
+      }
+    ]
+
+    const embed = new LilirucaEmbed()
+      .addFields(fields)
+
+    util.send(`\\🔧 ${ct('success')}`, embed)
+  }
+}
+
+module.exports = Technical
