894f90a34c0145052ed672ca222470c94331ce0a
Yslan Ramos <yslan3000@gmail.com>
2020-11-05 17:59:26 -0300

    test: command emojis test

--- src/__tests__/commands.test.js
+++ src/__tests__/commands.test.js
@@ -1,12 +1,20 @@
 const { CommandHandler, AkairoClient } = require('discord-akairo')
 const path = require('path')
 
-describe('Command Tests', () => {
+describe('Command Tests', async () => {
   const client = new AkairoClient()
   const commandHandler = new CommandHandler(client, {
     automateCategories: true,
     directory: path.join(__dirname, '..', 'commands')
   })
 
-  commandHandler.loadAll()
+  await commandHandler.loadAll()
+
+  // const categories = commandHandler.categories
+  const commands = commandHandler.modules.array()
+  const checkUniqueArr = arr => arr.some((v, i) => arr.indexOf(v) !== i)
+
+  test('test if have emojis duplicated', () => {
+    expect(checkUniqueArr(commands.map(c => c.emoji))).toBe(true)
+  })
 })
