eaeba742e8e698bf17f1fe7934593f035468dbe2
Gabriel Tavares <bieltavares-adm@hotmail.com>
2020-11-12 23:06:11 -0400

    Feat: Add option all energy in Wood Command

--- src/commands/production/wood.js
+++ src/commands/production/wood.js
@@ -2,7 +2,7 @@ const { Argument } = require('discord-akairo')
 const LilirucaCommand = require('@structures/LilirucaCommand')
 const LilirucaEmbed = require('@structures/LilirucaEmbed')
 const { random } = require('@utils/util')
-const { getItemName, getItem, getToolInInventory, removeItem, addItemInInventory } = require('@utils/items')
+const { getItemName, getItem, getToolInInventory, removeItem, addItemInInventory, addMultipleItemsInInventory } = require('@utils/items')
 const { ENERGY_COST, EMOJIS: { axe } } = require('@constants')
 
 class Wood extends LilirucaCommand {
@@ -24,21 +24,30 @@ class Wood extends LilirucaCommand {
         {
           id: 'item',
           type: 'item'
+        },
+        {
+          id: 'all',
+          match: 'flag',
+          flag: '--all'
         }
       ]
     })
   }
 
-  async exec ({ t, ct, util, db, author }, { uses, item }) {
+  async exec ({ t, ct, util, db, author }, { uses, item, all }) {
     const data = await db.users.get(author.id)
 
+    if (all) {
+      uses = Math.floor(data.energy / 10)
+    }
+
     const energyCost = ENERGY_COST * uses
-    if (data.energy < energyCost) {
+    if (!energyCost || data.energy < energyCost) {
       return util.send(t('errors:noEnergy'))
     }
 
     if (item && !data.items[item.id]) {
-      return ct('noAxe')
+      return util.send(ct('noAxe'))
     }
 
     const tool = item || getToolInInventory(data, 'axe')
@@ -66,10 +75,14 @@ class Wood extends LilirucaCommand {
       }
     ]
 
+    const stats = {
+      wood: amount,
+      [tool.id]: uses
+    }
+
     removeItem(data, 'items', tool.id, uses)
     addItemInInventory(data, 'items', 'wood', amount)
-    addItemInInventory(data, 'statistics', 'wood', amount)
-    addItemInInventory(data, 'statistics', tool.id, uses)
+    addMultipleItemsInInventory(data, 'statistics', stats)
 
     const values = {
       energy: data.energy - energyCost
