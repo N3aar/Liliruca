a39ec06f11e9d97f18ca529ed6a04d16daf44982
Yslan Ramos <yslan3000@gmail.com>
2020-11-23 19:15:34 -0300

    add args

--- src/structures/command/CommandArgument.js
+++ src/structures/command/CommandArgument.js
@@ -1,7 +1,7 @@
 
 const REST_FLAG_OPTION_REGEX = flags => new RegExp(`\\s--(${flags})\\s(.{1,})(--)?`, 'i')
 const PHRASE_FLAG_OPTION_REGEX = flags => new RegExp(`\\s--(${flags})\\s(\\w{1,})(\\s)`, 'i')
-// const FLAG_REGEX = flags => new RegExp(`\\s(--${flags})\\s`, 'gi')
+const FLAG_REGEX = flags => new RegExp(`\\s?--(${flags})`, 'i')
 
 module.exports = class CommandArgument {
   static getFlagOption (content, flagOptionArg) {
@@ -18,36 +18,64 @@ module.exports = class CommandArgument {
     return { newContent, res }
   }
 
-  static async getFlagOptionArgs (flagOptionArgs, content, handle) {
+  static getFlag (content, flagArg) {
+    const regex = FLAG_REGEX(flagArg.flags.join('|'))
+
+    if (!regex.test(content)) {
+      return false
+    }
+
+    const newContent = content.replace(regex, '')
+    return { newContent, res: true }
+  }
+
+  // HANDLE PARA BUSCA O HANDLE E OPTIONS DO ARGUMENTO
+  static getArgRunner () {
+    return null
+  }
+
+  // HANDLE PARA EXECUTA UM ARGUMENT INDIVITUAL
+  // EXECUÇÃO DIRENTE CASO O ARG.TYPE SEJA UMA ARRAY
+  static execArg (res, arg) {
+    return null
+  }
+
+  static async runFlags (content, flags, handle) {
     let currentContent = content
     const result = {}
 
-    for (const flagArg of flagOptionArgs) {
-      const res = CommandArgument.getFlagOption(currentContent, flagArg)
+    for (const flag of flags) {
+      const res = flag.flagType === 'option'
+        ? CommandArgument.getFlagOption(currentContent, flag)
+        : CommandArgument.getFlag(currentContent, flag)
 
-      result[flagArg.id] = await handle(res?.res ?? null)
+      result[flag.id] = await handle(res?.res ?? res, flag)
       currentContent = res?.newContent ?? currentContent
     }
 
     return { newContent: currentContent, result }
   }
 
-  // runArguments (commandArgs, messageArgs, message) {
-  //   const result = {}
-  //   // SEPARAR CADA PARTE EM FUNÇÕES E JA IR TESTANDO
-  //   // TODO: separa o propriedade arg nessas 4 no command
-  //   const { unorderedArgs, flags, flagOption, args } = commandArgs
-  //     .reduce((p, v) => {
-  //       if (v.match === 'flag') {
-  //         return { ...p, flags: [...p.flags, v] }
-  //       }
-  //       if (v.match === 'option') {
-  //         return { ...p, flagOption: [...p.flagOption, v] }
-  //       }
-  //       if (v.unordered) {
-  //         return { ...p, unordered: [...p.unordered, v] }
-  //       }
-  //       return { ...p, args: [...p.args, v] }
-  //     }, { unorderedArgs: [], flags: [], flagOption: [], args: [] })
-  // }
+  static async runArgs (content, args, handle) {
+    const contentArgs = content.trim().split(' ')
+    const result = {}
+
+    for (const arg of args) {
+      const lastIndex = arg.match === 'rest' ? contentArgs.length : 1
+      const text = contentArgs.slice(0, lastIndex)?.join(' ') ?? null
+      result[arg.id] = await handle(text, arg)
+      contentArgs.splice(0, lastIndex)
+    }
+
+    return result
+  }
+
+  static async runParameters (contentArgs, commandFlags, commandArgs) {
+    const {
+      newContent,
+      res
+    } = await CommandArgument.runFlags(contentArgs.join(' '), commandFlags, CommandArgument.execArg)
+
+    return { ...res, ...CommandArgument.runArgs(newContent.split(' '), commandArgs, CommandArgument.execArg) }
+  }
 }
--- src/structures/command/__tests__/CommandArgument.test.js
+++ src/structures/command/__tests__/CommandArgument.test.js
@@ -1,49 +1,74 @@
 const CommandArgument = require('../CommandArgument')
 
-const handle = arg => arg
-describe('CommandArgument#getFlagOptionArgs', () => {
-  it('should return only "ww"', async () => {
-    expect(await CommandArgument.getFlagOptionArgs(
-      [{
-        id: 'a',
-        flags: ['a', 'ate'],
-        match: 'phrase'
-      }],
+describe('CommandArgument#getFlagOption', () => {
+  it('should return only "ww"', () => {
+    expect(CommandArgument.getFlagOption(
       'aw dfe --a ww d',
-      handle
+      {
+        id: 'a',
+        flags: ['a', 'ate']
+      }
     )).toEqual({
       newContent: 'aw dfe d',
-      result: { a: 'ww' }
+      res: 'ww'
     })
   })
 
-  it('should return "ww a" only', async () => {
-    expect(await CommandArgument.getFlagOptionArgs(
-      [{
+  it('should return "ww a" only', () => {
+    expect(CommandArgument.getFlagOption(
+      'aw dfe --a ww d',
+      {
         id: 'a',
         flags: ['a', 'ata'],
         match: 'rest'
-      }],
-      'aw dfe --a ww d',
-      handle
+      }
     )).toEqual({
       newContent: 'aw dfe',
-      result: { a: 'ww d' }
+      res: 'ww d'
     })
   })
 
-  it('should return null', async () => {
-    expect(await CommandArgument.getFlagOptionArgs(
-      [{
+  it('should return null', () => {
+    expect(CommandArgument.getFlagOption(
+      'aw dfe --a ww d',
+      {
         id: 'a',
         flags: ['w', 'word'],
         match: 'rest'
-      }],
-      'aw dfe --a ww d',
-      handle
-    )).toEqual({
-      newContent: 'aw dfe --a ww d',
-      result: { a: null }
-    })
+      }
+    )).toEqual(null)
   })
+})
+
+describe('CommandArgument#getFlag', () => {
+  const flag = {
+    id: 'a',
+    flags: ['a', 'ate']
+  }
+
+  it('should return true', () => {
+    expect(CommandArgument.getFlag('aw dfe --a ww d', flag))
+      .toEqual({ newContent: 'aw dfe ww d', res: true })
+    expect(CommandArgument.getFlag('--a aw dfe ww d', flag))
+      .toEqual({ newContent: ' aw dfe ww d', res: true })
+    expect(CommandArgument.getFlag('aw dfe ww d --a', flag))
+      .toEqual({ newContent: 'aw dfe ww d', res: true })
+  })
+
+  it('should return false', () => {
+    expect(CommandArgument.getFlag('aw dfe a ww d', flag)).toBe(false)
+    expect(CommandArgument.getFlag(' aw a dfe  ww d', flag)).toBe(false)
+    expect(CommandArgument.getFlag('aw dfe  ww d a', flag)).toBe(false)
+  })
+})
+
+test('CommandArgument#runArgs', async () => {
+  const handle = arg => arg.length ? arg : null
+
+  const args = [{ id: 'a' }, { id: 'b' }]
+  expect(await CommandArgument.runArgs('aw dfe a ww d', args, handle)).toEqual({ a: 'aw', b: 'dfe' })
+  const arg2 = [{ id: 'a' }, { id: 'b', match: 'rest' }]
+  expect(await CommandArgument.runArgs(' aw a dfe  ww d', arg2, handle)).toEqual({ a: 'aw', b: 'a dfe  ww d' })
+  const arg3 = [{ id: 'a', match: 'rest' }, { id: 'b' }]
+  expect(await CommandArgument.runArgs('aw dfe  ww d a', arg3, handle)).toEqual({ a: 'aw dfe  ww d a', b: null })
 })
